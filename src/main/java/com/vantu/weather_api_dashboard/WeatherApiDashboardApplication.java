c460fc137265633b25bd07563dd02b38501d207c
vantu <tulevan526@gmail.com>
2025-10-05 09:54:08 +0700

    first commit

---
+++ src/main/java/com/vantu/weather_api_dashboard/WeatherApiDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.vantu.weather_api_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vantu/weather_api_dashboard/WeatherApiDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantu.weather_api_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
